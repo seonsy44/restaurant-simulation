b2113a0ca3769e0091e24311e882c993423d2eec
seon <seonsy44@gmail.com>
2023-05-24 16:20:20 +0900

    span el 오타 수정

--- widget/span.js
+++ widget/span.js
@@ -2,7 +2,7 @@ import { widget } from "./baseWidget.js";
 
 function _createSpan(id, option) {
   var el = document.createElement("span");
-  el.textContent = oprion.textContent;
+  el.textContent = option.textContent;
 
   return {
     id: id,
