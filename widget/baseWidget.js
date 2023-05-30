31e80c9ac6d190d0dc3a4890fb05409e06c63d0d
seon <seonsy44@gmail.com>
2023-05-30 20:46:14 +0900

    쓰지 않는 코드 삭제

--- widget/baseWidget.js
+++ widget/baseWidget.js
@@ -1,15 +1,5 @@
 import { addControl, delControl, getControl, hasControl } from "./core.js";
 
-function chaining(id, _func) {
-  var func = function () {
-    _func(arguments);
-
-    return getControl(id);
-  };
-
-  return func;
-}
-
 export function widget(_createControl) {
   var createControl = function (id, option) {
     if (hasControl(id)) throw new Error(`id: ${id}는 이미 존재`);
